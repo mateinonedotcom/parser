44174f639776775ff013a1a77fe2b12c9fbcaa38
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-11 21:35:35 +0100

    Read games for the 2012/12/31

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mateinonedotcom/parser
 
 go 1.22
+
+require github.com/notnil/chess v1.9.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/notnil/chess"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+
+	f, err := os.Open("pgn/lichess/2012/12/31/lichess-2012-12-31.pgn")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	scanner := chess.NewScanner(f)
+	for scanner.Scan() {
+		game := scanner.Next()
+
+		fmt.Println(game)
+	}
 }

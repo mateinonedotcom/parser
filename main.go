a55d40582de192ae8820876edd332570a299863a
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-11 22:40:09 +0100

    Identify checkmate move and board

--- main.go
+++ main.go
@@ -20,7 +20,28 @@ func main() {
 	scanner := chess.NewScanner(f)
 	for scanner.Scan() {
 		game := scanner.Next()
+		if game.Method() != chess.Checkmate {
+			continue
+		}
 
-		fmt.Println(game)
+		moveHistory := game.MoveHistory()
+		moveLast := moveHistory[len(moveHistory)-1]
+
+		fmt.Println(moveLast.PrePosition.Board().Draw())
+		fmt.Println("Turn: ", moveLast.PrePosition.Turn())
+		fmt.Println("Moves: ", moveLast.PrePosition.ValidMoves())
+		fmt.Println("")
+
+		checkmates := []*chess.Move{}
+		for _, move := range moveLast.PrePosition.ValidMoves() {
+			position := moveLast.PrePosition.Update(move)
+			if position.Status() == chess.Checkmate {
+				checkmates = append(checkmates, move)
+			}
+		}
+
+		fmt.Println("Checkmates: ", checkmates)
+
+		break
 	}
 }
